e55c6a7d4cf9494367497d9345d35499a82635cf
EvgeniySZ <zhuk_es@mail.ru>
2024-07-15 00:37:04 +0300

    1407HomeAtNight

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 from .validators import real_age
 
+from django.urls import reverse
+
 
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
@@ -11,6 +13,8 @@ class Birthday(models.Model):
     )
     birthday = models.DateField('Дата рождения', validators=(real_age,))
     image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
+
+
     class Meta:
         constraints = (
             models.UniqueConstraint(
@@ -18,3 +22,8 @@ class Birthday(models.Model):
                 name='Unique person constraint',
             ),
         )
+
+
+    def get_absolute_url(self):
+        # С помощью функции reverse() возвращаем URL объекта.
+        return reverse('birthday:detail', kwargs={'pk': self.pk}) 
